d80bb2d35b90ca968dcd9b1d2a3ce0d5395e0b71
Jeffery Kimbrow <71730781+KimbrowSkilledKC@users.noreply.github.com>
2020-10-27 09:53:31 -0500

    ok

--- JS/fizzBuzz.js
+++ JS/fizzBuzz.js
@@ -1,11 +1,15 @@
 //I need to create a function to write the output of FizzBuzz to HTML
 
 document.getElementById("innerTextOutput").onclick = function changeContent() {
+    // on a click event the content needs to change to the results of my function inside of the Id "innerTextOutput"
+
     var sum;
+
     var i;
+
     sum = 0;
 
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 0; i <= 100; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
             document.getElementById("innerTextOutput").innerHTML += ('FizzBuzz \n');
         } else if (i % 3 === 0) {
