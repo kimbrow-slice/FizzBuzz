223e481e51908d9988811a3e334b70c2d37dbb85
Jeffery Kimbrow <71730781+KimbrowSkilledKC@users.noreply.github.com>
2020-10-29 10:33:29 -0500

    making code more readable
    declared and assigned the value of node. value of document.getElementById("innerTextOutput") to the const ouput to not be as redundant

--- JS/fizzBuzz.js
+++ JS/fizzBuzz.js
@@ -6,22 +6,25 @@ document.getElementById("innerTextOutput").onclick = function changeContent() {
 
 
     var i;
+
     //declaring my variable i
 
+    const output = document.getElementById("innerTextOutput");
+    //declaring and assigning the const output to the value of document.getElementById("innerTextOutput") I am doing this to make the code more readable and being less repetitive with the node value of document.getElementById("innerTextOutput")
 
     //start for loop
-    for (let i = 1; i <= 1000; i++) { //begin with i = 1; my condition is i<= 500; the step is i++ or the postfix form
+    for (let i = 1; i <= 1000; i++) { //begin with i = 1; my condition is i<= 1000; the step is i++ or the postfix form
         //the body of the conidtion, if conidition -> run body and run step
         if (i % 3 === 0 && i % 5 === 0) {
-            document.getElementById("innerTextOutput").innerHTML += ('FizzBuzz \n');
+            output.innerHTML += ('FizzBuzz \n');
         } else if (i % 3 === 0) {
-            document.getElementById("innerTextOutput").innerHTML += ('Fizz \n');
+            output.innerHTML += ('Fizz \n');
         } else if (i % 5 === 0) {
-            document.getElementById("innerTextOutput").innerHTML += ('Buzz \n');
+            output.innerHTML += ('Buzz \n');
         } else {
-            document.getElementById("innerTextOutput").innerHTML += (i + '\n');
-            //document.getElementById("innerTextOutput").innerHTML += (i + '\n') means the following
-            //document.getElementById("innerTextOutput") returns an Element object representing the element whose id property matches the specified string. 
+            output.innerHTML += (i + '\n');
+            //output.innerHTML += (i + '\n') means the following
+            //output.innerHTML returns an Element object representing the element whose id property matches the specified string. 
             //.innerHTML is a property used to get or set the HTML content of an element node. It is a part of tthe Document Object Model that allows JavaScript code to manipulate a website being displayed 
             // += adds the value of the right operand to a variable and assigns the result to the variable. In this situation we are using concatenation
             // I had to insert '\n' into the string for the function to add a new line or else it would of been displayed as follows
